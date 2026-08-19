e611144a911641c5e149e9d8f2b37826ab9f0df0
mac <@b.tunde.ipaye@gmail.com>
2026-08-19 04:19:39 +0100

    get api config updated commit by ENGR. IPAYE

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use axum::{
 #[tokio::main]
 async fn main(){
     // build our application with a single route
-    let app = Router::new().route("/", get(|| async {"Hello, world!"}));
+    let app = Router::new().route("/", get(|| async {"Hello, world hi!"}));
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
