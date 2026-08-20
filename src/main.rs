8057bc8943afa952448d23a918b15f63f48d0f0a
mac <@b.tunde.ipaye@gmail.com>
2026-08-20 16:25:23 +0100

    feat: conn, database updated commit by ENGR. IPAYE

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,11 @@ use axum::{
 
 #[tokio::main]
 async fn main(){
+    std::fs::create_dir_all("data");
     // build our application with a single route
-    let app = Router::new().route("/", get(|| async {"Welcome to the backend CRUD API Hey Ipaye are you there "}));
+    let connection: Connection = sqlite::open("data/todo.db").unwrap();
+
+    let app = Router::new().route("/", get(|| async {"Welcome to the backend CRUD API! Hey, Ipaye are you there "}));
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
