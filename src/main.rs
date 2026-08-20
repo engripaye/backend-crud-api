4c8255283ae256275a0d499af390623c765f1b23
mac <@b.tunde.ipaye@gmail.com>
2026-08-20 16:18:48 +0100

    feat: main rs updated commit by ENGR. IPAYE

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use axum::{
 #[tokio::main]
 async fn main(){
     // build our application with a single route
-    let app = Router::new().route("/", get(|| async {"Welcome to the backend CRUD API"}));
+    let app = Router::new().route("/", get(|| async {"Welcome to the backend CRUD API Hey Ipaye are you there "}));
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
